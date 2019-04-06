0e8c0c2398626a1978bd756df1a5a77ad1149612
Kye Berkeland <kyeberkeland@yahoo.com>
2019-04-05 20:10:56 -0500

    add todolist

--- src/Todo/TodoMain.js
+++ src/Todo/TodoMain.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import TodoList from './TodoList.js';
 
+let arrayOne = [3,5,6,3,2];
+
+
+    
 class TodoMain extends Component {
+
     render() {
         return (
             <div>
+                <p>Todo List</p>
+                
             </div>
         )
     }
